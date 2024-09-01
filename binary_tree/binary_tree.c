b65ef400e09f20a72eb489ada528d857a273da52
MacroShadow <macroshadow@foxmail.com>
2024-09-01 06:27:54 -0400

    tree: add binary tree traversal function

--- binary_tree/binary_tree.c
+++ binary_tree/binary_tree.c
@@ -8,6 +8,7 @@ struct node_data {
 };
 
 BINTREE_HEAD(bin_tree, node_data);
+BINTREE_GENERATE(bin_tree, node_data, entry);
 struct bin_tree btree = BINTREE_INITIALIZER;
 
 struct node_data *bin_tree_left_child_build(struct node_data *base)
@@ -42,6 +43,13 @@ struct node_data *bin_tree_right_child_build(struct node_data *base)
     return child;
 }
 
+/** Example:
+ *                          0
+ *                         / \
+ *                        1   2
+ *                       / \   \
+ *                      2   3   4
+ */
 void bin_tree_build(void)
 {
     struct node_data *root =
@@ -60,15 +68,116 @@ void bin_tree_build(void)
     bin_tree_right_child_build(rc);
 }
 
+void preorder_traversal_r(struct node_data *node)
+{
+    if (node == NULL) {
+        return;
+    }
+    printf("id: %d\n", node->id);
+
+    preorder_traversal_r(node->entry.bte_left);
+    preorder_traversal_r(node->entry.bte_right);
+}
+
+struct node_data *inorder_traversal_begin()
+{
+    struct node_data *node = NULL;
+    struct node_data *parent = NULL;
+    // traverse along left branch until the leaf
+    node = BINTREE_ROOT(&btree);
+
+    while (node) {
+        parent = node;
+        node = BINTREE_LEFT(node, entry);
+    }
+
+    return parent;
+}
+
+struct node_data *inorder_traversal_next(struct node_data *node)
+{
+    if (BINTREE_RIGHT(node, entry)) {
+        node = BINTREE_RIGHT(node, entry);
+        // traverse along left branch until the leaf
+        while (BINTREE_LEFT(node, entry)) {
+            node = BINTREE_LEFT(node, entry);
+        }
+    } else {
+        // backtrack the parent of the left child
+        if (BINTREE_PARENT(node, entry) &&
+            node == BINTREE_LEFT(BINTREE_PARENT(node, entry), entry)) {
+            node = BINTREE_PARENT(node, entry);
+        } else {
+            // backtrack the ancestor of the right child where is not traversed
+            while (BINTREE_PARENT(node, entry) &&
+                   node == BINTREE_RIGHT(BINTREE_PARENT(node, entry), entry)) {
+                node = BINTREE_PARENT(node, entry);
+            }
+            node = BINTREE_PARENT(node, entry);
+        }
+    }
+
+    return node;
+}
+
+void inorder_traversal_iter(void) {
+    struct node_data *node = NULL;
+
+    BINTREE_INORDER_FOREACH(bin_tree, &btree, node) {
+        printf("id: %d\n", node->id);
+    }
+}
+
+void inorder_traversal_r(struct node_data *node)
+{
+    if (node == NULL) {
+        return;
+    }
+
+    inorder_traversal_r(node->entry.bte_left);
+    printf("id: %d\n", node->id);
+    inorder_traversal_r(node->entry.bte_right);
+}
+
+void inorder_traversal(void) {
+    struct node_data *root = BINTREE_ROOT(&btree);
+
+    printf("inorder recursion traversal: \n");
+    inorder_traversal_r(root);
+
+    printf("inorder iterator traversal: \n");
+    inorder_traversal_iter();
+}
+
+void postorder_traversal_r(struct node_data *node)
+{
+    if (node == NULL) {
+        return;
+    }
+
+    postorder_traversal_r(node->entry.bte_left);
+    postorder_traversal_r(node->entry.bte_right);
+    printf("id: %d\n", node->id);
+}
+
+void bin_tree_traversal_r(void)
+{
+    struct node_data *root = BINTREE_ROOT(&btree);
+
+    printf("preorder recursion traversal: \n");
+    preorder_traversal_r(root);
+
+    printf("inorder traversal: \n");
+    inorder_traversal();
+
+    printf("postorder recursion traversal: \n");
+    postorder_traversal_r(root);
+}
+
 int main()
 {
     BINTREE_INIT(&btree);
     bin_tree_build();
-    struct node_data *root = BINTREE_ROOT(&btree);
-    printf("root id: %d\n", root->id);
-    struct node_data *root_lc = BINTREE_LEFT(root, entry);
-    printf("root left child id: %d\n", root_lc->id);
-    struct node_data *root_rc = BINTREE_RIGHT(root, entry);
-    printf("root right child id: %d\n", root_rc->id);
+    bin_tree_traversal_r();
     return 0;
 }
--- include/binary_tree.h
+++ include/binary_tree.h
@@ -8,9 +8,9 @@
 
 #define BINTREE_ENTRY(type)                                                    \
     struct {                                                                   \
-        struct type *parent;                                                   \
-        struct type *left;                                                     \
-        struct type *right;                                                    \
+        struct type *bte_parent;                                               \
+        struct type *bte_left;                                                 \
+        struct type *bte_right;                                                \
     }
 
 // clang-format off
@@ -22,9 +22,10 @@
         (head)->bth_root = NULL;                                               \
     } while (0)
 
-#define BINTREE_ROOT(head)         (head)->bth_root
-#define BINTREE_LEFT(base, field)  (base)->field.left
-#define BINTREE_RIGHT(base, field) (base)->field.right
+#define BINTREE_ROOT(head)          (head)->bth_root
+#define BINTREE_LEFT(node, field)   (node)->field.bte_left
+#define BINTREE_RIGHT(node, field)  (node)->field.bte_right
+#define BINTREE_PARENT(node, field) (node)->field.bte_parent
 
 #define BINTREE_INSERT_ROOT(head, root)                                        \
     do {                                                                       \
@@ -33,16 +34,72 @@
         }                                                                      \
     } while (0)
 
-#define BINTREE_INSERT_AS_LC(head, base, lc, field)                            \
+#define BINTREE_INSERT_AS_LC(head, parent, lc, field)                          \
     do {                                                                       \
-        (base)->field.left = lc;                                               \
-        (lc)->field.parent = base;                                             \
+        (parent)->field.bte_left = lc;                                         \
+        (lc)->field.bte_parent = parent;                                       \
     } while (0)
 
-#define BINTREE_INSERT_AS_RC(head, base, rc, field)                            \
+#define BINTREE_INSERT_AS_RC(head, parent, rc, field)                          \
     do {                                                                       \
-        (base)->field.right = rc;                                              \
-        (rc)->field.parent = base;                                             \
+        (parent)->field.bte_right = rc;                                        \
+        (rc)->field.bte_parent = parent;                                       \
     } while (0)
+
+#define BINTREE_GENERATE_INORDER_BEGIN(name, type, field)                      \
+    struct type *name##_INORDER_BEGINE(struct name *head)                      \
+    {                                                                          \
+        struct type *tmp = NULL;                                               \
+        struct type *parent = NULL;                                            \
+        /* traverse along left branch until the leaf */                        \
+        tmp = BINTREE_ROOT(head);                                              \
+                                                                               \
+        while (tmp) {                                                          \
+            parent = tmp;                                                      \
+            tmp = BINTREE_LEFT(tmp, field);                                    \
+        }                                                                      \
+                                                                               \
+        return parent;                                                         \
+    }
+
+#define BINTREE_GENERATE_INORDER_NEXT(name, type, field)                       \
+    struct type *name##_INORDER_NEXT(struct type *node)                        \
+    {                                                                          \
+        if (BINTREE_RIGHT(node, field)) {                                      \
+            node = BINTREE_RIGHT(node, field);                                 \
+            /* traverse along left branch until the leaf */                    \
+            while (BINTREE_LEFT(node, field)) {                                \
+                node = BINTREE_LEFT(node, field);                              \
+            }                                                                  \
+        } else {                                                               \
+            /* backtrack the parent of the left child */                       \
+            if (BINTREE_PARENT(node, field) &&                                 \
+                node == BINTREE_LEFT(BINTREE_PARENT(node, field), field)) {    \
+                node = BINTREE_PARENT(node, field);                            \
+            } else {                                                           \
+                /* backtrack the ancestor of the right child where is not      \
+                 * traversed */                                                \
+                while (BINTREE_PARENT(node, field) &&                          \
+                       node == BINTREE_RIGHT(BINTREE_PARENT(node, field),      \
+                                             field)) {                         \
+                    node = BINTREE_PARENT(node, field);                        \
+                }                                                              \
+                node = BINTREE_PARENT(node, field);                            \
+            }                                                                  \
+        }                                                                      \
+                                                                               \
+        return node;                                                           \
+    }
+
+#define BINTREE_GENERATE(name, type, field)                                    \
+    BINTREE_GENERATE_INORDER_BEGIN(name, type, field)                          \
+    BINTREE_GENERATE_INORDER_NEXT(name, type, field)
+
+#define BINTREE_INORDER_BEGIN(name, head) name##_INORDER_BEGINE(head)
+#define BINTREE_INORDER_NEXT(name, node)  name##_INORDER_NEXT(node)
+
+#define BINTREE_INORDER_FOREACH(name, head, var)                               \
+    for (var = BINTREE_INORDER_BEGIN(name, head); var != NULL;                 \
+         var = BINTREE_INORDER_NEXT(name, var))
 
 #endif
